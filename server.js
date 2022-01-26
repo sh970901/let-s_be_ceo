c0b6cab3167035dac42d1a88842f5bfbcddcf3ee
sh970901 <aaddss639@naver.com>
2022-01-26 20:22:59 +0900

    setting

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+const mysql = require('mysql');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data)
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+  });
+  connection.connect();
+
+app.get('/api/hello', (req,res)=>{
+    connection.query('SELECT * FROM addlogin', function(err,rows,fields){
+        res.header("Access-Control-Allow-Origin", "*");
+        res.send(rows)
+    })
+})
+
+app.listen(port, ()=> console.log("서버 작동"))
