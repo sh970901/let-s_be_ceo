3cd8a3a16fa9832f4d4874608b1e3a028148f477
sh970901 <aaddss639@naver.com>
2022-03-07 18:25:48 +0900

    빈 공간 처리

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,6 @@ import BoardDetail from './components/Board/BoardDetail';
 import AddBoard from './components/Board/AddBoard';
 import MyBoard from './components/Board/MyBoard';
 
-
 function App() {
   return (
     <BrowserRouter>
