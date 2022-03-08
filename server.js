ff6d0f3c06671b3a623f103f2396e3663d741484
sh970901 <aaddss639@naver.com>
2022-03-08 18:28:59 +0900

    server

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const mysql = require('mysql');
 
 const app = express();
 const port = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
