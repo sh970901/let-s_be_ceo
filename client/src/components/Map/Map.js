e25e6915066e8a7532474491dfdd36b5adc6a0e9
sh970901 <aaddss639@naver.com>
2022-03-18 00:22:21 +0900

    멀티폴리곤 수정

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,186 +1,178 @@
 /*global kakao*/
 import React, { useEffect, useState } from 'react'
 
-import { Geojson } from './Geojson';
 
 //카카오맵 정보
-const infowindow = new kakao.maps.InfoWindow({ removable: true, width: 500 });
 
 const Map = (props) => {
-    // const [word, setWord] = useState("기본값");
-    useEffect(() => {
-        let data = props.geojson[0].features
-        let coordinates = [];
-        let name = '';
-        let polygons = [];
-        console.log(data)
-        let container = document.getElementById("map");
-        let options = {
-            center: new kakao.maps.LatLng(37.5557334, 126.8502565),
-            level: 6,
-        };
-        const map = new kakao.maps.Map(container, options);
-        const customOverlay = new kakao.maps.CustomOverlay({})
-        const infowondow = new kakao.maps.InfoWindow({ removable: true });
+  let data = props.geojson[0].features
+  useEffect(() => {
+   
+    // initialization(map)
+    DrawPolygon()
+  }, [])
+  function initialization(_map){
+    map=_map
+  }
+  function DrawPolygon() {
+    var name = "";
+    data.forEach((coordinate) => {
+      name = coordinate.name
+      if (coordinate.geometry.type === "MultiPolygon") {
+        displayArea(coordinate.geometry.coordinates,true)
+        console.log(coordinate.geometry.coordinates)
+      }
+      else {
+        displayArea(coordinate.geometry.coordinates, false)
+      }
+    })
+  }
+  function makePolygon(coordinates) {
+    var polygonPath = [];
+    coordinates.forEach((coordinate) => {
+      polygonPath.push(new kakao.maps.LatLng(coordinate[1], coordinate[0]));
+    });
+    return new kakao.maps.Polygon({
+      path: polygonPath,
+      strokeWeight: 2, // 선의 두께입니다
+      strokeColor: '#004c80', // 선의 색깔입니다
+      strokeOpacity: 0.8, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+      strokeStyle: 'solid', // 선의 스타일입니다
+      fillColor: '#fff', // 채우기 색깔입니다
+      fillOpacity: 0.7, // 채우기 불투명도 입니다
+    })
+  }
 
-        const displayArea = (coordinates, name) => {
-            let path = [];
-            let points = [];
-            // let areaResult = pollution.filter((item) => item[0] === name);
-            // console.log(areaResult);
+  function makeMultiPolygon(coordinates){
+    var polygonPath = [];
 
-            coordinates[0].forEach((coordinate)=>{
-            let point = {};
-            point.x = coordinate[1];
-            point.y = coordinate[0];
-            points.push(point);
-            path.push(new kakao.maps.LatLng(coordinate[1], coordinate[0]));
-            });
+    coordinates.forEach((val2)=>{
+      var coordinates2 =[];
 
-            let polygon = new kakao.maps.Polygon({
-                map: map,
-                path: path, // 그려질 다각형의 좌표 배열입니다
-                strokeWeight: 2, // 선의 두께입니다
-                strokeColor: '#004c80', // 선의 색깔입니다
-                strokeOpacity: 0.8, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-                strokeStyle: 'solid', // 선의 스타일입니다
-                fillColor: '#fff', // 채우기 색깔입니다
-                fillOpacity: 0.7, // 채우기 불투명도 입니다
-              });
-              polygons.push(polygon);
+      val2.forEach((coordinate)=>{
+        coordinates2.push(new kakao.maps.LatLng(coordinate[1], coordinate[0]));
+      })
+      polygonPath.push(coordinates2)
+    });
+    return new kakao.maps.Polygon({
+      path: polygonPath,
+      strokeWeight: 2, // 선의 두께입니다
+      strokeColor: '#004c80', // 선의 색깔입니다
+      strokeOpacity: 0.8, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+      strokeStyle: 'solid', // 선의 스타일입니다
+      fillColor: '#fff', // 채우기 색깔입니다
+      fillOpacity: 0.7, // 채우기 불투명도 입니다
+    })
+  }
 
-              // 다각형에 mouseover 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 변경합니다
-      // 지역명을 표시하는 커스텀오버레이를 지도위에 표시합니다
-      kakao.maps.event.addListener(polygon, 'mouseover', function (mouseEvent) {
-        polygon.setOptions({ fillColor: '#09f' });
+  function displayArea(coordinates, multi){
+    let container = document.getElementById("map");
+    let options = {
+      center: new kakao.maps.LatLng(37.5557334, 126.8502565),
+      level: 6,
+    };
+    const map = new kakao.maps.Map(container, options);
+    var polygon;
+    if(multi){
+      polygon = makeMultiPolygon(coordinates);
+    }
+    else{
+      polygon = makePolygon(coordinates);
+    }
+    polygon.setMap(map);
+  }
 
-        customOverlay.setContent('<div class="area">' + name + '</div>');
-
-        customOverlay.setPosition(mouseEvent.latLng);
-        customOverlay.setMap(map);
-      });
-
-      // 다각형에 mousemove 이벤트를 등록하고 이벤트가 발생하면 커스텀 오버레이의 위치를 변경합니다
-      kakao.maps.event.addListener(polygon, 'mousemove', function (mouseEvent) {
-        customOverlay.setPosition(mouseEvent.latLng);
-      });
-
-      // 다각형에 mouseout 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 원래색으로 변경합니다
-      // 커스텀 오버레이를 지도에서 제거합니다
-      kakao.maps.event.addListener(polygon, 'mouseout', function () {
-        polygon.setOptions({ fillColor: '#fff' });
-        customOverlay.setMap(null);
-      });
-
-      // 다각형에 click 이벤트를 등록하고 이벤트가 발생하면 다각형의 이름과 면적을 인포윈도우에 표시합니다
-      kakao.maps.event.addListener(polygon, 'click', function (mouseEvent) {
-        const content ='<div>dd</div>'
-          
-
-        infowindow.setContent(content);
-        infowindow.setPosition(mouseEvent.latLng);
-        infowindow.setMap(map);
-      });
-
-                  
-        }
-        data.forEach((val)=>{
-            coordinates = val.geometry.coordinates;
-            name = val.name
-            console.log(name)
-    
-            displayArea(coordinates, name)
-        })
-
-    }, [])
+  // const [word, setWord] = useState("기본값");
 
 
-    // function view(map) {
-    //     for (var i = 0, len = props.location.length; i < len; i++) {
-    //         displayArea(map, props.location[i], props);
-    //     }
-    // }
-    // useEffect(() => {
-    //     infowindow.close()
-    // }, [])
-    // useEffect(() => {
-    //     console.log(props.place)
-    // }, [props.place])
 
-    // function displayArea(map, area, props ) {
+  // function view(map) {
+  //     for (var i = 0, len = props.location.length; i < len; i++) {
+  //         displayArea(map, props.location[i], props);
+  //     }
+  // }
+  // useEffect(() => {
+  //     infowindow.close()
+  // }, [])
+  // useEffect(() => {
+  //     console.log(props.place)
+  // }, [props.place])
 
-    //     const customOverlay = new kakao.maps.CustomOverlay({})
-    //     //다각형 생성
-    //     var polygon = new kakao.maps.Polygon({
-    //         map: map, // 다각형을 표시할 지도 객체
-    //         path: area.path,
-    //         strokeWeight: 2,
-    //         strokeColor: '#004c80',
-    //         strokeOpacity: 0.8,
-    //         fillColor: '#fff',
-    //         fillOpacity: 0.7
-    //     });
-    //     // 다각형에 mouseover 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 변경 
-    //     // 지역명을 표시하는 커스텀오버레이를 지도위에 표시
-    //     kakao.maps.event.addListener(polygon, 'mouseover', function (mouseEvent) {
-    //         polygon.setOptions({ fillColor: '#09f' });
+  // function displayArea(map, area, props ) {
 
-    //         customOverlay.setContent('<div class="area">' + area.name + '</div>');
+  //     const customOverlay = new kakao.maps.CustomOverlay({})
+  //     //다각형 생성
+  //     var polygon = new kakao.maps.Polygon({
+  //         map: map, // 다각형을 표시할 지도 객체
+  //         path: area.path,
+  //         strokeWeight: 2,
+  //         strokeColor: '#004c80',
+  //         strokeOpacity: 0.8,
+  //         fillColor: '#fff',
+  //         fillOpacity: 0.7
+  //     });
+  //     // 다각형에 mouseover 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 변경 
+  //     // 지역명을 표시하는 커스텀오버레이를 지도위에 표시
+  //     kakao.maps.event.addListener(polygon, 'mouseover', function (mouseEvent) {
+  //         polygon.setOptions({ fillColor: '#09f' });
 
-    //         customOverlay.setPosition(mouseEvent.latLng);
-    //         // customOverlay.setMap(map);
-    //     });
+  //         customOverlay.setContent('<div class="area">' + area.name + '</div>');
 
-    //     // 다각형에 mousemove 이벤트를 등록하고 이벤트가 발생하면 커스텀 오버레이의 위치를 변경합니다 
-    //     kakao.maps.event.addListener(polygon, 'mousemove', function (mouseEvent) {
+  //         customOverlay.setPosition(mouseEvent.latLng);
+  //         // customOverlay.setMap(map);
+  //     });
 
-    //         customOverlay.setPosition(mouseEvent.latLng);
+  //     // 다각형에 mousemove 이벤트를 등록하고 이벤트가 발생하면 커스텀 오버레이의 위치를 변경합니다 
+  //     kakao.maps.event.addListener(polygon, 'mousemove', function (mouseEvent) {
 
-    //     });
+  //         customOverlay.setPosition(mouseEvent.latLng);
 
-    //     // 다각형에 mouseout 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 원래색으로 변경합니다
-    //     // 커스텀 오버레이를 지도에서 제거합니다 
-    //     kakao.maps.event.addListener(polygon, 'mouseout', function () {
-    //         polygon.setOptions({ fillColor: '#fff' });
-    //         customOverlay.setMap(null);
+  //     });
 
-    //     });
+  //     // 다각형에 mouseout 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 원래색으로 변경합니다
+  //     // 커스텀 오버레이를 지도에서 제거합니다 
+  //     kakao.maps.event.addListener(polygon, 'mouseout', function () {
+  //         polygon.setOptions({ fillColor: '#fff' });
+  //         customOverlay.setMap(null);
 
-    //     // 다각형에 click 이벤트를 등록하고 이벤트가 발생하면 다각형의 이름과 면적을 인포윈도우에 표시합니다 
-    //     kakao.maps.event.addListener(polygon, 'click', function (mouseEvent) {
-    //         // var content =
-    //         //   '<div class="info">' +
-    //         //   '   <div class="title">' + area.name + '</div>' +
-    //         //   '   <div class="size">총 면적 : 약 ' + Math.floor(polygon.getArea()) + ' m<sup>2</sup></div>' +
-    //         //   '<div> <input type="button" onclick="location.href=\'api?locate=\'"+"area.name" value="정보보기"></div>' +
-    //         //   '</div>';
-    //         var content =
-    //             '<div class="info" style="width:300px; height:100px;padding:6px 0;">' +
-    //             '<input id="dn" type="text" value="값">' +
-    //             '   <div class="title">' + area.name + '</div>' +
-    //             '   <div class="size">총 면적 : 약 ' + Math.floor(polygon.getArea()) + ' m<sup>2</sup></div>' +
-    //             //'<button type="button" id=\'btn1\' onclick="document.getElementById(\'dn\').value=\'111!\'">' + area.name + '</btton>' +
-    //             // '<button type="button" id=\'btn1\' onclick="console.log(document.getElementById(\'dn\').value)">' + area.name + '</btton>' +
-    //             // '<button type="button" id= \'btn1\'></button>'+
-    //             '<button type="button" onclick="console.log()"></button>' +
-    //             '<button type="button" id= \'btn1\'></button>' +
-    //             '</div>';
-    //         props.setPlace(area.name)
+  //     });
 
-    //         infowindow.setContent(content)
-    //         infowindow.setPosition(mouseEvent.latLng);
-    //         infowindow.setMap(map);
-    //     });
+  //     // 다각형에 click 이벤트를 등록하고 이벤트가 발생하면 다각형의 이름과 면적을 인포윈도우에 표시합니다 
+  //     kakao.maps.event.addListener(polygon, 'click', function (mouseEvent) {
+  //         // var content =
+  //         //   '<div class="info">' +
+  //         //   '   <div class="title">' + area.name + '</div>' +
+  //         //   '   <div class="size">총 면적 : 약 ' + Math.floor(polygon.getArea()) + ' m<sup>2</sup></div>' +
+  //         //   '<div> <input type="button" onclick="location.href=\'api?locate=\'"+"area.name" value="정보보기"></div>' +
+  //         //   '</div>';
+  //         var content =
+  //             '<div class="info" style="width:300px; height:100px;padding:6px 0;">' +
+  //             '<input id="dn" type="text" value="값">' +
+  //             '   <div class="title">' + area.name + '</div>' +
+  //             '   <div class="size">총 면적 : 약 ' + Math.floor(polygon.getArea()) + ' m<sup>2</sup></div>' +
+  //             //'<button type="button" id=\'btn1\' onclick="document.getElementById(\'dn\').value=\'111!\'">' + area.name + '</btton>' +
+  //             // '<button type="button" id=\'btn1\' onclick="console.log(document.getElementById(\'dn\').value)">' + area.name + '</btton>' +
+  //             // '<button type="button" id= \'btn1\'></button>'+
+  //             '<button type="button" onclick="console.log()"></button>' +
+  //             '<button type="button" id= \'btn1\'></button>' +
+  //             '</div>';
+  //         props.setPlace(area.name)
 
-    // }
+  //         infowindow.setContent(content)
+  //         infowindow.setPosition(mouseEvent.latLng);
+  //         infowindow.setMap(map);
+  //     });
+
+  // }
 
 
-    return (
-        <div>
-            <h1>상권분석</h1>
-            <div id="map" style={{ width: "70vw", height: "100vh" }}></div>
-        </div>
-    )
+  return (
+    <div>
+
+      <h1>상권분석</h1>
+      <div id="map" style={{ width: "70vw", height: "100vh" }}></div>
+    </div>
+  )
 }
 
 export default Map
