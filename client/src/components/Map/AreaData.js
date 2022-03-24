826919dce5f91b8bd1db8f7aaa905d8fc3ecc7af
sh970901 <aaddss639@naver.com>
2022-03-24 20:31:01 +0900

    server.js 강남구 변형

--- client/src/components/Map/AreaData.js
+++ client/src/components/Map/AreaData.js
@@ -1,11 +1,39 @@
 import React from 'react'
 import { useEffect } from 'react'
+import { Pie } from "react-chartjs-2";
+import { MDBContainer } from "mdbreact";
+
 
 const AreaData = (props) => {
     useEffect(()=>{
         
     },[])
-
+const state = {
+  dataPie: {
+    labels: ["RED", "Green", "Yellow", "Grey", "Dark Grey"],
+    datasets:[
+      {
+        data: [300, 50, 100, 40, 120],
+          backgroundColor: [
+            "#F7464A",
+            "#46BFBD",
+            "#FDB45C",
+            "#949FB1",
+            "#4D5360",
+            "#AC64AD"
+          ],
+          hoverBackgroundColor: [
+            "#FF5A5E",
+            "#5AD3D1",
+            "#FFC870",
+            "#A8B3C5",
+            "#616774",
+            "#DA92DB"
+          ]
+      }
+    ]
+  }
+}
 
     function showData(){
       console.log(props.areaData)
@@ -15,6 +43,9 @@ const AreaData = (props) => {
     <div>
       AreaData
       <button onClick={showData}>정보보기</button>
+      <MDBContainer>
+        <Pie data={state.dataPie} options={{responsive: true}}></Pie>
+      </MDBContainer>
     </div>
   )
 }
